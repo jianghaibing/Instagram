22411ec12b0ab6af7cf57af580ce1ee761bf2928
jianghaibing <jianghaibing83@gmail.com>
2015-11-10 21:29:01 +0800

    完善上传头像

--- Instagram/RegestViewController.swift
+++ Instagram/RegestViewController.swift
@@ -14,11 +14,10 @@ class RegestViewController: UITableViewController,UIImagePickerControllerDelegat
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var regestItem: UIBarButtonItem!
+    @IBOutlet weak var onePhotoLabel: UILabel!
+    
     var imageFile:AVFile?
-    
-    var emailSting:String = ""
-    
-    var tempImgID:String?
+    var emailSting:String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -79,27 +78,49 @@ class RegestViewController: UITableViewController,UIImagePickerControllerDelegat
     @IBAction func choosePhoto(sender: UIButton) {
         let imagePickerVC = UIImagePickerController()
         imagePickerVC.delegate = self
+        
+        imagePickerVC.modalPresentationStyle = .CurrentContext
+        imagePickerVC.allowsEditing = true
         imagePickerVC.navigationBar.tintColor = UIColor.whiteColor()
         imagePickerVC.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.whiteColor()]
         imagePickerVC.navigationBar.barTintColor = globalColor
-        self.presentViewController(imagePickerVC, animated: true, completion: nil)
+        
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+        actionSheet.view.tintColor = globalColor
+        let takePhotoAction = UIAlertAction(title: "拍照", style: UIAlertActionStyle.Default) { (action) -> Void in
+            imagePickerVC.sourceType = .Camera
+            self.presentViewController(imagePickerVC, animated: true, completion: nil)
+        }
+        let photoAction = UIAlertAction(title: "相册", style: .Default) { (action) -> Void in
+            imagePickerVC.sourceType = .SavedPhotosAlbum
+            self.presentViewController(imagePickerVC, animated: true, completion: nil)
+        }
+        let cancel = UIAlertAction(title: "取消", style: .Cancel, handler: nil)
+        actionSheet.addAction(takePhotoAction)
+        actionSheet.addAction(photoAction)
+        actionSheet.addAction(cancel)
+        
+        if UIImagePickerController.isCameraDeviceAvailable(.Rear) || UIImagePickerController.isCameraDeviceAvailable(.Front){
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+        }else{
+            imagePickerVC.sourceType = .SavedPhotosAlbum
+            self.presentViewController(imagePickerVC, animated: true, completion: nil)
+        }
+        
     }
     
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        guard let rfURL = info[UIImagePickerControllerReferenceURL] else {return}
-        let urlString = rfURL.description
-        let imgID = urlString.componentsSeparatedByString("id=").last?.componentsSeparatedByString("&").first
-        let shortID = imgID?.substringFromIndex((imgID?.endIndex.advancedBy(-4))!)//截取ID的后4位拼接到文件名
-        if tempImgID != imgID {
-            let hud = MBProgressHUD.showHUDAddedTo(picker.view, animated: true)
-            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        let hud = MBProgressHUD.showHUDAddedTo(picker.view, animated: true)
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage{
             let imageData = UIImageJPEGRepresentation(image, 0.7)
-            imageFile = AVFile(name: "avatar_\(shortID!).jpg", data: imageData)
+            imageFile = AVFile(name: "avatar.jpg", data: imageData)
             imageFile!.saveInBackgroundWithBlock({ (sucessed, error) -> Void in
                 if sucessed == true {
                     hud.hide(true)
                     self.addPhotoButton.setImage(image, forState: .Normal)
+                    self.onePhotoLabel.hidden = true
                     self.dismissViewControllerAnimated(true, completion: nil)
                 }else{
                     hud.hide(true)
@@ -110,13 +131,11 @@ class RegestViewController: UITableViewController,UIImagePickerControllerDelegat
                     self.dismissViewControllerAnimated(true, completion: nil)
                 }
             })
-            tempImgID = imgID
-        }else{
-            self.dismissViewControllerAnimated(true, completion: nil)
         }
-        
-        
+
     }
+    
+    
     
     /*
     // MARK: - Navigation
